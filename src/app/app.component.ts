388438a057d09ba46febb8ed0d9926b705c042a4
Máté Illés <mate.illes@centricdigital.com>
2018-01-02 13:37:27 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent {
   //   this.state = (this.state === 'small' ? 'large' : 'small');
   // }
   //
-  // title: string = "";
+  title: string = "APP";
   //
   // ngOnInit() {
   //   console.log(this.dataService.cars);
